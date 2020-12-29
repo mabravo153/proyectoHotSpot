45bad5bd476f11764b7bfc3d1fb7a5d42614f557
Carlos Bravo <cbravomartinez01@gmail.com>
2020-12-29 13:07:30 -0500

    Pestaña TyC y bloqueo24Horas

---
+++ TyC.php
@@ -0,0 +1,44 @@
+<?php
+
+include('template/header.php');
+
+?>
+<header>
+    <img class="logo" src="img/alcaldia.jpg" alt="Imagen Alcaldia">
+</header>
+<main class="contenedor">
+
+    <h2 class="text-center encabezado-terminos">Aviso de privacidad</h2>
+
+
+    <div class="row">
+
+
+        <div class="espaciado-normas">
+
+            <p>La Alcaldía Distrital de Barranquilla en cumplimiento de la Ley 1581 de 2012 y demás normas concordantes, es responsable del tratamiento de sus datos personales.</p>
+            <p><strong>Los datos personales que la Alcaldía Distrital de Barranquilla solicita serán utilizados para los siguientes fines:</strong></p>
+            <ul>
+                <li>Adelantar los trámites y servicios de todas las dependencias de la Alcaldía Distrital de Barranquilla, en ejercicio de sus funciones.</li>
+                <li>Dar a conocer los servicios ofrecidos por la Alcaldía Distrital de Barranquilla.</li>
+                <li>Participar en eventos organizados por la Alcaldía Distrital de Barranquilla.</li>
+                <li>Participar en capacitaciones, cursos, seminarios organizados por la Alcaldía Distrital de Barranquilla.</li>
+                <li>Invitar a eventos de participación ciudadana.</li>
+                <li>Evaluar la calidad de los servicios de la Alcaldía Distrital de Barranquilla.</li>
+            </ul>
+            <p>Como titular de información tiene derecho a conocer, actualizar y rectificar sus datos personales y, sólo en los casos en que sea procedente, a suprimirlos o revocar la autorización otorgada para su tratamiento.</p>
+            <p>Si requiere más información para conocer nuestra política de tratamiento de datos personales y los cambios sustanciales que se produzcan en ella, consúltela en nuestra página web: <a href="https://www.barranquilla.gov.co/">www.barranquilla.gov.co</a> </p>
+            <p>Si desea presentar una consulta, reclamo o petición de información relacionada con la protección de datos personales puede ingresar a la página web de la Alcaldía Distrital de Barranquilla, <a href="https://www.barranquilla.gov.co/">www.barranquilla.gov.co</a> en la opción Atención al Ciudadano o al correo electrónico atencionalciudadano@barranquilla.gov.co</p>
+            <p>También puede comunicarse a los teléfonos +57 (5) 3399888 – 3399889 o en presencial en la Sede Principal Calle 34 No. 43-31 primer piso.</p>
+        </div>
+    </div>
+
+
+</main>
+
+
+<?php
+
+include('template/footer.php')
+
+?>
---
+++ bloqueo24Horas.php
@@ -0,0 +1,32 @@
+<?php
+
+include('template/header.php');
+
+?>
+<header>
+    <img class="logo" src="img/alcaldia.jpg" alt="Imagen Alcaldia">
+</header>
+<main class="contenedor">
+
+    <h2 class="text-center encabezado-terminos">Navegacion Denegada</h2>
+
+
+    <div class="row">
+
+
+        <div class="espaciado-normas">
+
+            <p>Por politicas de la Alcaldía Distrital de Barranquilla el usuario debe esperar 24 horas para volver a hacer uso del servicio.</p>
+            
+        </div>
+    </div>
+
+
+</main>
+
+
+<?php
+
+include('template/footer.php')
+
+?>
